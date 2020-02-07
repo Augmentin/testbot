d2428faa7afc583c1ee62cb61879c998e27835d1
Augmentin <anohov2012@yandex.ru>
2020-02-07 13:04:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(content_types=['text'])
 
 def lalalal(message):
-	bot.send_message(message.chat.id, 'хуй'+message.text)
+	bot.send_message(message.chat.id, message.text)
 
 # RUN
 bot.polling(none_stop=True)
